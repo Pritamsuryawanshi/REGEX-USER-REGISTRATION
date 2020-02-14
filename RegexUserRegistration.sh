1be0eefcb2ccc3c0431eb06a6b2845c274de1416
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-14 11:14:11 +0530

    ADDED: REGEX pattern for valid email id

--- RegexUserRegistration.sh
+++ RegexUserRegistration.sh
@@ -5,19 +5,23 @@ echo "Welcome to REGEX user registration exercises"
 
 read -p "Enter your first name with first letter as CAPITAL: " firstName
 read -p "Enter your last name with first letter as CAPITAL: " lastName
+read -p "Enter your email ID: " emailId
 
-<<<<<<< HEAD
-firstNamePattern="^([A-Z]{1}[a-zA-Z]{2,})$"
-
-if [[ $firstName =~  $firstNamePattern ]]
-=======
 firstNamePattern="^([A-Z]{1}[a-zA-Z]*)$"
 lastNamePattern=$firstNamePattern
+emailIdPattern="^([a-zA-Z]{3,}[0-9a-zA-Z_.-+]*[@]{1}[0-9a-zA-Z]*[.]{1}[a-zA-Z]{2,4}[.]*[a-zA-Z]*)$"
 
 if [[ $firstName =~  $firstNamePattern && $lastName =~  $lastNamePattern ]]
->>>>>>> UC2_Getting_Valid_Last_Name
 then
 	echo "VALID"
 else
 	echo "INVALID"
 fi
+
+if [[ $emailId =~ $emailIdPattern ]]
+then
+	echo "VALID email"
+else
+	echo "INVALID email"
+fi
+
