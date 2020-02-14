f3a15bca157acd5ebcf1b55f0bbba0d638f1c605
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-14 11:31:15 +0530

    ADDED: REGEX pattern for password by rule one

--- RegexUserRegistration.sh
+++ RegexUserRegistration.sh
@@ -7,12 +7,13 @@ read -p "Enter your first name with first letter as CAPITAL: " firstName
 read -p "Enter your last name with first letter as CAPITAL: " lastName
 read -p "Enter your email ID: " emailId
 read -p "Enter your Phone number: " phoneNumber
+read -p "Enter the password: " password
 
 firstNamePattern="^([A-Z]{1}[a-zA-Z]*)$"
 lastNamePattern=$firstNamePattern
 emailIdPattern="^([a-zA-Z]{3,}[0-9a-zA-Z_.-+]*[@]{1}[0-9a-zA-Z]*[.]{1}[a-zA-Z]{2,4}[.]*[a-zA-Z]*)$"
 phoneNumberPattern="^[0-9]{2}[[:space:]][0-9]{10}$"
-
+passwordPattern="^[0-9a-zA-Z]{8,}$"
 if [[ $firstName =~  $firstNamePattern && $lastName =~  $lastNamePattern ]]
 then
 	echo "VALID"
@@ -32,4 +33,11 @@ then
 	echo "VALID PHONE NUMBER"
 else
 	echo "INVALID PHONE NUMBER"
+fi
+
+if [[ $password =~ $passwordPattern ]]
+then
+	echo "VALID PASSWORD"
+else
+	echo "INVALID PASSWORD"
 fi
