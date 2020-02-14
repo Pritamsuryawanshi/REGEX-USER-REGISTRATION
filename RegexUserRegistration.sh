84693e70521d02fad7d45340af5cca6a83a62d83
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-14 11:23:35 +0530

    ADDED: REGEX pattern for valid phone number

--- RegexUserRegistration.sh
+++ RegexUserRegistration.sh
@@ -6,10 +6,12 @@ echo "Welcome to REGEX user registration exercises"
 read -p "Enter your first name with first letter as CAPITAL: " firstName
 read -p "Enter your last name with first letter as CAPITAL: " lastName
 read -p "Enter your email ID: " emailId
+read -p "Enter your Phone number: " phoneNumber
 
 firstNamePattern="^([A-Z]{1}[a-zA-Z]*)$"
 lastNamePattern=$firstNamePattern
 emailIdPattern="^([a-zA-Z]{3,}[0-9a-zA-Z_.-+]*[@]{1}[0-9a-zA-Z]*[.]{1}[a-zA-Z]{2,4}[.]*[a-zA-Z]*)$"
+phoneNumberPattern="^[0-9]{2}[[:space:]][0-9]{10}$"
 
 if [[ $firstName =~  $firstNamePattern && $lastName =~  $lastNamePattern ]]
 then
@@ -20,8 +22,14 @@ fi
 
 if [[ $emailId =~ $emailIdPattern ]]
 then
-	echo "VALID email"
+	echo "VALID EMAIL ID"
 else
-	echo "INVALID email"
+	echo "INVALID EMAIL ID"
 fi
 
+if [[ $phoneNumber =~ $phoneNumberPattern ]]
+then
+	echo "VALID PHONE NUMBER"
+else
+	echo "INVALID PHONE NUMBER"
+fi
