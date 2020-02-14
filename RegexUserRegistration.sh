ad4e8626cd60709d22cf4c301f536e2d919c0346
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-14 10:41:25 +0530

    MODIFIED: added spaces

--- RegexUserRegistration.sh
+++ RegexUserRegistration.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 shopt -s extglob
 
-echo "Welcome REGEX user registration exercises"
+echo "Welcome to REGEX user registration exercises"
 
 read -p "Enter the your name with first letter as CAPITAL: " firstName
+
 firstNamePattern="^([A-Z]{1}[a-zA-Z]*)$"
 if [[ $firstName =~  $firstNamePattern ]]
 then
