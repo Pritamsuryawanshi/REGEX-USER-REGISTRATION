5165cc3dd472ef82e8590f1ee88d7c0a2a4c7d5f
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-14 10:37:00 +0530

    ADDED: REGEX pattern for valid first name

--- RegexUserRegistration.sh
+++ RegexUserRegistration.sh
@@ -2,3 +2,12 @@
 shopt -s extglob
 
 echo "Welcome REGEX user registration exercises"
+
+read -p "Enter the your name with first letter as CAPITAL: " firstName
+firstNamePattern="^([A-Z]{1}[a-zA-Z]*)$"
+if [[ $firstName =~  $firstNamePattern ]]
+then
+	echo "VALID"
+else
+	echo "INVALID"
+fi
