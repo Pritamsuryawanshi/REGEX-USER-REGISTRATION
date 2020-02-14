bb8996a91fca12f80d485e544fa0e85ea6762ce3
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-02-14 10:56:37 +0530

    ADDED: REGEX pattern for valid last name

--- RegexUserRegistration.sh
+++ RegexUserRegistration.sh
@@ -1,12 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash 
 shopt -s extglob
 
 echo "Welcome to REGEX user registration exercises"
 
-read -p "Enter the your name with first letter as CAPITAL: " firstName
+read -p "Enter your first name with first letter as CAPITAL: " firstName
+read -p "Enter your last name with first letter as CAPITAL: " lastName
 
 firstNamePattern="^([A-Z]{1}[a-zA-Z]*)$"
-if [[ $firstName =~  $firstNamePattern ]]
+lastNamePattern=$firstNamePattern
+
+if [[ $firstName =~  $firstNamePattern && $lastName =~  $lastNamePattern ]]
 then
 	echo "VALID"
 else
